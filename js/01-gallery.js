ad19d7b16e1a03c1661f5a7085ca44e9f12271e3
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-09 00:43:09 +0300

    Image gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listGallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function renderMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+listGallery.insertAdjacentHTML("beforeend", renderMarkup(galleryItems));
+listGallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+	    <img
+            src="${event.target.dataset.source}"
+            alt="${event.target.alt}"
+        />
+`);
+
+  instance.show();
+}
