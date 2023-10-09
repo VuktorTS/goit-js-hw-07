aef4dca69e05b153d936e8aad749038d7ae48c8b
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-09 11:52:45 +0300

    SimpleLightbox library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+
+function renderMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+listGallery.insertAdjacentHTML("beforeend", renderMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captions: true,
+  captionDelay: 250,
+});
